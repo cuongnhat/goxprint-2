a858d007522b2e739aa742998c5da526cd144f38
cuongnhat <83998388+cuongnhat@users.noreply.github.com>
2025-12-26 16:29:19 +0700

    UP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -16,26 +14,8 @@ export default defineConfig(({ mode }) => {
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       build: {
-        lib: {
-          entry: 'index.tsx',
-          fileName: 'index',
-          formats: ['es']
-        },
-        rollupOptions: {
-          external: ['react', 'react-dom'],
-          output: {
-            globals: {
-              react: 'React',
-              'react-dom': 'ReactDOM'
-            }
-          }
-        },
-        target: 'esnext'
-      },
-      resolve: {
-        alias: {
-          '@': resolve(fileURLToPath(new URL('.', import.meta.url)), '.'),
-        }
+        outDir: 'dist',
+        assetsDir: 'assets'
       }
     };
 });

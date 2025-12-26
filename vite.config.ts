c489c5830b6cade7b77a4c862144ea2d09548383
cuongnhat <83998388+cuongnhat@users.noreply.github.com>
2025-12-26 16:25:10 +0700

    cf

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -14,9 +15,26 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      build: {
+        lib: {
+          entry: 'index.tsx',
+          fileName: 'index',
+          formats: ['es']
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM'
+            }
+          }
+        },
+        target: 'esnext'
+      },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': resolve(fileURLToPath(new URL('.', import.meta.url)), '.'),
         }
       }
     };
